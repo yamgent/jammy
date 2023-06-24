85e039714261df798a2716812d192e80b58b60ab
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-06-24 15:31:51 +0800

    Loading splash screen (#1)
    * Add empty jammy_splash_screen
    
    * Add jammy_splash_screen implementation
    
    * Add metadata
    
    * Add license files
    
    * Add version dependency for splash_screen
    
    * Remove unnecessary next_state
    
    * Fix missing docs
    
    * Add build_and_test CI
    
    * Fix ci.yml
    
    * Add clippy CI

---
+++ examples/splash_screen.rs
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use bevy::prelude::*;
+use iyes_progress::{Progress, ProgressPlugin, ProgressSystem};
+use jammy::splash_screen::JammySplashScreenPlugin;
+
+const SHORT_TASK_SECS: usize = 1;
+const MEDIUM_TASK_SECS: usize = 5;
+const LONG_TASK_SECS: usize = 10;
+
+#[derive(States, Default, Debug, Hash, PartialEq, Eq, Clone)]
+enum AppState {
+    #[default]
+    Loading,
+    Done,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
+        .add_plugin(ProgressPlugin::new(AppState::Loading).continue_to(AppState::Done))
+        .add_plugin(JammySplashScreenPlugin::new(AppState::Loading))
+        .add_system(
+            fake_task::<SHORT_TASK_SECS>
+                .track_progress()
+                .run_if(in_state(AppState::Loading)),
+        )
+        .add_system(
+            fake_task::<MEDIUM_TASK_SECS>
+                .track_progress()
+                .run_if(in_state(AppState::Loading)),
+        )
+        .add_system(
+            fake_task::<LONG_TASK_SECS>
+                .track_progress()
+                .run_if(in_state(AppState::Loading)),
+        )
+        .run();
+}
+
+fn fake_task<const TIME: usize>(time: Res<Time>, mut logged: Local<bool>) -> Progress {
+    if time.elapsed_seconds() > TIME as f32 {
+        if !*logged {
+            info!("Task with duration {TIME} complete!");
+            *logged = true;
+        }
+        true.into()
+    } else {
+        false.into()
+    }
+}
